986a0b0851f4276db2122a8ab648163115694b60
luisrafaelinf <luisrafaelinf@gmail.com>
2018-10-31 11:20:31 -0400

    Task 4 resolved, stats pages by users

--- src/Controller/IndexAction.php
+++ src/Controller/IndexAction.php
@@ -5,6 +5,7 @@ namespace Snowdog\DevTest\Controller;
 use Snowdog\DevTest\Model\User;
 use Snowdog\DevTest\Model\UserManager;
 use Snowdog\DevTest\Model\WebsiteManager;
+use Snowdog\DevTest\Model\PageManager;
 
 class IndexAction
 {
@@ -15,13 +16,20 @@ class IndexAction
     private $websiteManager;
 
     /**
+     * @var PageManager
+     */
+    private $pageManager;
+
+    /**
      * @var User
      */
     private $user;
 
-    public function __construct(UserManager $userManager, WebsiteManager $websiteManager)
+    public function __construct(UserManager $userManager, WebsiteManager $websiteManager, PageManager $pageManager)
     {
         $this->websiteManager = $websiteManager;
+        $this->pageManager = $pageManager;
+
         if (isset($_SESSION['login'])) {
             $this->user = $userManager->getByLogin($_SESSION['login']);
         }
@@ -31,7 +39,7 @@ class IndexAction
     {
         if($this->user) {
             return $this->websiteManager->getAllByUser($this->user);
-        } 
+        }
         return [];
     }
 
@@ -39,4 +47,22 @@ class IndexAction
     {
         require __DIR__ . '/../view/index.phtml';
     }
+
+    public function getTotalPagesUser()
+    {
+        return $this->pageManager->totalPagesByUser($this->user);
+    }
+
+    public function getLeastPageVisitedByUser()
+    {
+        $pageUrl = $this->pageManager->leastRecentlyVisitedByUser($this->user);
+        return $pageUrl ? $pageUrl : '';
+    }
+
+    public function getMostPageVisitedByUser()
+    {
+        $pageUrl = $this->pageManager->mostRecentlyVisitedByUser($this->user);
+        return $pageUrl ? $pageUrl : '';
+    }
+
 }
--- src/Model/PageManager.php
+++ src/Model/PageManager.php
@@ -47,18 +47,68 @@ class PageManager
 
         $websiteId = $statement->fetchColumn();
 
-        //if ($websiteId !== NULL) {
+        $dateTime = new \DateTime('NOW');
+        $dateFormat = $dateTime->format('Y-m-d H:i:s');
+        $statement = $this->database->prepare('UPDATE pages set date_time_last_view = :now where url = :url AND website_id = :websiteId');
+        $statement->bindParam(':now', $dateFormat, \PDO::PARAM_STR);
+        $statement->bindParam(':url', $url, \PDO::PARAM_STR);
+        $statement->bindParam(':websiteId', $websiteId, \PDO::PARAM_INT);
+        $statement->execute();
 
-            $dateTime = new \DateTime('NOW');
-            $dateFormat = $dateTime->format('Y-m-d H:i:s');
-            $statement = $this->database->prepare('UPDATE pages set date_time_last_view = :now where url = :url AND website_id = :websiteId');
-            $statement->bindParam(':now', $dateFormat, \PDO::PARAM_STR);
-            $statement->bindParam(':url', $url, \PDO::PARAM_STR);
-            $statement->bindParam(':websiteId', $websiteId, \PDO::PARAM_INT);
-            $statement->execute();
-
-            return $statement->rowCount();
-        //}
+        return $statement->rowCount();
 
     }
+
+    public function totalPagesByUser(User $user)
+    {
+        $userId = $user->getUserId();
+
+        $statement = $this->database->prepare('
+                    SELECT count(p.page_id) total
+                    FROM pages p
+                    inner join websites w on w.website_id = p.website_id
+                    where w.user_id = :userId
+                ');
+        $statement->bindParam(':userId', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return intval($statement->fetchColumn());
+    }
+
+    public function leastRecentlyVisitedByUser(User $user)
+    {
+        $userId = $user->getUserId();
+
+        $statement = $this->database->prepare('
+                        select p.url
+                        from pages p
+                        inner join websites w on w.website_id = p.website_id
+                        where w.user_id = :userId
+                        order by date_time_last_view asc
+                        limit 1
+                    ');
+        $statement->bindParam(':userId', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return (string) $statement->fetchColumn();
+    }
+
+    public function mostRecentlyVisitedByUser(User $user)
+    {
+        $userId = $user->getUserId();
+
+        $statement = $this->database->prepare('
+                        select p.url
+                        from pages p
+                        inner join websites w on w.website_id = p.website_id
+                        where w.user_id = :userId
+                        order by date_time_last_view desc
+                        limit 1
+                    ');
+        $statement->bindParam(':userId', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return (string) $statement->fetchColumn();
+    }
+
 }
